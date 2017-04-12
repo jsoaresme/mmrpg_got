188247d5205d3781c653145f0d6307625ce7178e
jsoaresme <jsoaresme@gmail.com>
2017-04-12 15:03:15 -0300

    Adicionando o Sair. Adicionando imasgens das casas dimanicas

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,8 +1,14 @@
 module.exports.jogo = function(application, req, res){
 	if(req.session.autorizado){
-		res.render('jogo');	
+		res.render('jogo', {img_casa: req.session.casa});	
 	} else {
 		res.send('Usuário precisa fazer login');
-	}
-	
+	}	
+}
+
+module.exports.sair = function(application, req, res){
+	req.session.destroy(function(err){
+		res.render("index", {validacao:{}});
+	});
+
 }
